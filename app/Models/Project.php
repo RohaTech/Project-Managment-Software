3caefff556eefcf20c402bfb40d8f86b54452568
Dawithabte31 <dawithabitamu31@gmail.com>
2024-08-13 01:06:25 +0300

    "Updated Eloquent models: added fillable fields, casts, and relationships to ActivityLog, Attachment, Message, Project, ProjectMember, SubTask, Task, and User models."

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,32 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Project extends Model
 {
     use HasFactory;
-
-    // The table associated with the model.
     protected $table = 'projects';
-
-    // The primary key associated with the table.
     protected $primaryKey = 'project_id';
-
-    // The attributes that are mass assignable.
     protected $fillable = [
         'name',
-        'description',
-        'created_by',
+        'content',
+        'user_id',
     ];
 
-    // The attributes that should be cast to native types.
     protected $casts = [
         'created_at' => 'datetime',
     ];
-
-    //Define any relationships here if needed, e.g., user
     public function creator()
-    {
+    {                   
         return $this->belongsTo(User::class, 'created_by');
     }
+    public function projects()
+    {
+        return $this->hasMany(Task::class, 'project_Id');
+    }
+
 }
