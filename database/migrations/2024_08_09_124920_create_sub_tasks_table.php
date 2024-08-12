f2c54e35cae459f98de1da1f237e33a60c26a612
RohaTech <raklovetocode@gmail.com>
2024-08-12 11:14:26 +0300

    Add task_id as foreign key to sub_task table migration

--- database/migrations/2024_08_09_124920_create_sub_tasks_table.php
+++ database/migrations/2024_08_09_124920_create_sub_tasks_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('sub_tasks', function (Blueprint $table) {
             $table->id();
             $table->string("name");
+            $table->foreignId("task_id")->constrained('tasks')->onDelete("cascade");
             $table->foreignId("created_by")->constrained('users')->onDelete("cascade");
             $table->foreignId("updated_by")->constrained('users')->onDelete("cascade");
             $table->string("status")->default("pending")->nullable();
