62a349a54902d1bdd77b7a8cfebdd12618e003e7
RohaTech <raklovetocode@gmail.com>
2024-09-18 08:37:37 +0300

    try to add response to auto reload

--- resources/js/Pages/Project/ApproveButton.jsx
+++ resources/js/Pages/Project/ApproveButton.jsx
@@ -1,21 +1,35 @@
-import React from "react";
 import { useForm } from "@inertiajs/react";
+import { useState } from "react";
+import { useEffect } from "react";
 
 export default function ApproveButton({ ApproveData, task }) {
   const { data, setData, patch, processing, errors, reset } = useForm({
     approve: "",
   });
+
+  const [approved, setApproved] = useState(ApproveData.approved);
+
+  useEffect(() => {
+    console.log(approved);
+  }, [approved]);
+
   const approve = () => {
     setData("approve", 1);
-    patch(route("task.approve", [task]));
+    patch(route("task.approve", [task]), {
+      onSuccess: (response) => {
+        const filterdtask = response.props.tasks.filter(
+          (item) => item.id === task.id
+        );
+
+        setApproved(filterdtask.approved);
+      },
+    });
   };
   return (
     <button
       onClick={approve}
       class={`${
-        ApproveData.approved || ApproveData.status === "Not Started"
-          ? "hidden"
-          : ""
+        approved || ApproveData.status === "Not Started" ? "hidden" : ""
       }`}
     >
       Approve
